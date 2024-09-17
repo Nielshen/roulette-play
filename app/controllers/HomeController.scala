ff277d01ef16b2947452d9353f409dd9d44dfa68
Nielshen <Niels.hensler@gmail.com>
2024-09-17 16:29:11 +0200

    Html rules page routing

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -21,4 +21,8 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
+
+  def rules() = Action { implicit request: Request[AnyContent] =>
+  Ok(views.html.rules())
+  }
 }
